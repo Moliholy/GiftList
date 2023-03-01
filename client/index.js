269b799afb97c36da7a052b300a67c4f3fb075aa
José Molina <jose.molina@criptan.com>
2023-03-02 00:33:31 +0100

    complete the task

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const {keccak256} = require("ethereum-cryptography/keccak");
+const {bytesToHex, utf8ToBytes} = require("ethereum-cryptography/utils");
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const randomNumber = Math.floor(Math.random() * niceList.length);
+  const hashes = niceList.map(gift => bytesToHex(keccak256(utf8ToBytes(gift))));
+  const merkleTree = new MerkleTree(hashes);
+  const proof = merkleTree.getProof(randomNumber);
+  const root = merkleTree.getRoot();
+  console.log('The root is ', root);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    gift: niceList[randomNumber],
+    proof
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const {keccak256} = require("ethereum-cryptography/keccak");
+const {utf8ToBytes, bytesToHex} = require("ethereum-cryptography/utils");
 
 const port = 1225;
 
@@ -8,15 +10,14 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'a8c425a3115617326b95149ce3681cadffffe75f9d301d4eaf067c19b4115020';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const {proof, gift} = req.body;
+  const leaf = bytesToHex(keccak256(utf8ToBytes(gift)));
+  const isInTheList = verifyProof(proof, leaf, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
